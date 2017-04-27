c38aa34a82b5a6f17a27e680890f8a260fbcf36f
OkelyKodely <dh.cho428@gmail.com>
2017-04-27 10:22:48 -0700

    Add files via upload

---
+++ MyPanel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Picture_Picture
+{
+    public class MyPanel : Panel
+    {
+        public Bitmap bitmap_, b_;
+
+        public List<Picture_Picture.PicturePicture.TBitmap> bitmap = new List<Picture_Picture.PicturePicture.TBitmap>();
+
+        public Picture_Picture.PicturePicture.Cursor _ = new Picture_Picture.PicturePicture.Cursor();
+
+        public Picture_Picture.PicturePicture.Cursor _empty = new Picture_Picture.PicturePicture.Cursor();
+
+        public MyPanel(Picture_Picture.PicturePicture.Cursor cursor, Bitmap b)
+        {
+            bitmap_ = b;
+            _ = cursor;
+            _empty.x = 375;
+            _empty.y = 375;
+        }
+
+        public void setup()
+        {
+            bitmap[0].x = 0;
+            bitmap[0].y = 0;
+            bitmap[1].x = 125;
+            bitmap[1].y = 0;
+            bitmap[2].x = 250;
+            bitmap[2].y = 0;
+            bitmap[3].x = 375;
+            bitmap[3].y = 0;
+
+            bitmap[4].x = 0;
+            bitmap[4].y = 125;
+            bitmap[5].x = 125;
+            bitmap[5].y = 125;
+            bitmap[6].x = 250;
+            bitmap[6].y = 125;
+            bitmap[7].x = 375;
+            bitmap[7].y = 125;
+
+            bitmap[8].x = 0;
+            bitmap[8].y = 250;
+            bitmap[9].x = 125;
+            bitmap[9].y = 250;
+            bitmap[10].x = 250;
+            bitmap[10].y = 250;
+            bitmap[11].x = 375;
+            bitmap[11].y = 250;
+            
+            bitmap[12].x = 0;
+            bitmap[12].y = 375;
+            bitmap[13].x = 125;
+            bitmap[13].y = 375;
+            bitmap[14].x = 250;
+            bitmap[14].y = 375;
+            bitmap[15].x = 375;
+            bitmap[15].y = 500;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            if (bitmap_ != null)
+            {
+                b_ = bitmap_;
+                bitmap_ = null;
+            }
+            else
+            {
+                bitmap_ = b_;
+                b_ = null;
+            }
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (bitmap_ != null)
+            {
+                e.Graphics.DrawImage(bitmap_, 0, 0, 500, 500);
+                e.Graphics.DrawString("Click Anywhere to Play", new Font("arial", 20f), new SolidBrush(Color.Black), 20, 540);
+            }
+            else
+            {
+                e.Graphics.DrawString("Click Anywhere to Preview", new Font("arial", 20f), new SolidBrush(Color.Black), 20, 540);
+                for (int i = 0; i < 16; i++)
+                    e.Graphics.DrawImage(bitmap[i].bitmap, bitmap[i].x, bitmap[i].y, 500 / 4, 500 / 4);
+                Rectangle rect = new Rectangle(_.x, _.y, 125, 125);
+                e.Graphics.DrawRectangle(new Pen(new SolidBrush(Color.White)), rect);
+            }
+        }
+    }
+}
---
+++ PicturePicture.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Picture_Picture
+{
+    public class PicturePicture : Form
+    {
+        MyPanel panel;
+        Graphics graphics;
+        List<Image> list = new List<Image>();
+
+        public class TBitmap
+        {
+            public int x, y;
+
+            public Bitmap bitmap;
+
+            public TBitmap(Image img)
+            {
+                bitmap = new Bitmap(img);
+            }
+        }
+
+        public class Cursor
+        {
+            public int x, y;
+        }
+
+        bool control = false;
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if(keyData == Keys.Control)
+            {
+                control = true;
+            }
+            if (keyData == Keys.Up)
+            {
+                int xx = panel._.x;
+                int yy = panel._.y;
+                for (int i = 0; i < panel.bitmap.Count; i++)
+                {
+                    if (panel.bitmap[i].x == xx && panel.bitmap[i].y == yy &&
+                        panel.bitmap[i].x == panel._empty.x && panel.bitmap[i].y - 125 == panel._empty.y)
+                    {
+                        panel.bitmap[i].y -= 125;
+                        panel._empty.y += 125;
+                    }
+                }
+                panel._.y -= 125;
+            }
+            else if (keyData == Keys.Down)
+            {
+                int xx = panel._.x;
+                int yy = panel._.y;
+                for (int i = 0; i < panel.bitmap.Count; i++)
+                {
+                    if (panel.bitmap[i].x == xx && panel.bitmap[i].y == yy &&
+                        panel.bitmap[i].x == panel._empty.x && panel.bitmap[i].y + 125 == panel._empty.y)
+                    {
+                        panel.bitmap[i].y += 125;
+                        panel._empty.y -= 125;
+                    }
+                }
+                panel._.y += 125;
+            }
+            else if (keyData == Keys.Left)
+            {
+                int xx = panel._.x;
+                int yy = panel._.y;
+                for (int i = 0; i < panel.bitmap.Count; i++)
+                {
+                    if (panel.bitmap[i].x == xx && panel.bitmap[i].y == yy &&
+                        panel.bitmap[i].x - 125 == panel._empty.x && panel.bitmap[i].y == panel._empty.y)
+                    {
+                        panel.bitmap[i].x -= 125;
+                        panel._empty.x += 125;
+                    }
+                }
+                panel._.x -= 125;
+            }
+            else if (keyData == Keys.Right)
+            {
+                int xx = panel._.x;
+                int yy = panel._.y;
+                for(int i=0; i<panel.bitmap.Count; i++)
+                {
+                    if (panel.bitmap[i].x == xx && panel.bitmap[i].y == yy &&
+                        panel.bitmap[i].x + 125 == panel._empty.x && panel.bitmap[i].y == panel._empty.y)
+                    {
+                        panel.bitmap[i].x += 125;
+                        panel._empty.x -= 125;
+                    }
+                }
+                panel._.x += 125;
+            }
+            panel.Invalidate();
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        public PicturePicture()
+        {
+            this.Text = "Sliding Puzzle";
+
+            Cursor _ = new Cursor();
+
+            Bitmap bitmap = new Bitmap(Environment.CurrentDirectory + "\\picture.gif");
+            panel = new MyPanel(_, bitmap);
+            graphics = panel.CreateGraphics();
+
+            this.SetBounds(0, 0, 1000, 800);
+            panel.SetBounds(0, 0, 1000, 800);
+            panel.BackColor = Color.Chartreuse;
+            //this.WindowState = FormWindowState.Maximized;
+
+            this.Controls.Add(panel);
+
+            Pen pen = new Pen(new SolidBrush(Color.Black));
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    Rectangle r = new Rectangle(i * (bitmap.Width / 4),
+                                                y * (bitmap.Height / 4),
+                                                bitmap.Width / 4,
+                                                bitmap.Height / 4);
+
+                    graphics.DrawRectangle(pen, r);
+
+                    list.Add(cropImage(bitmap, r));
+                }
+            }
+
+            for (int i = 0; i < 16; i++)
+                panel.bitmap.Add(new TBitmap(list[i]));
+
+            panel.setup();
+        }
+
+        private Image cropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea, System.Drawing.Imaging.PixelFormat.DontCare);
+            return (Image)(bmpCrop);
+        }    
+    }
+}
